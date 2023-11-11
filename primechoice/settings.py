44d8d269351bb92b78b04f60d8e99f898b79f9a3
x9x96 <israelbook@gmail.com>
2023-11-11 16:58:11 +0100

    :-)

--- primechoice/settings.py
+++ primechoice/settings.py
@@ -110,6 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'resources')
 MEDIA_URL = '/resources/'
